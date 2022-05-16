c367fdd524262117eed64b29fd7527b7625d10d9
HackerShark <melsharkawi@mitre.org>
2022-05-16 16:03:26 -0400

    fixing netlify issues
    Signed-off-by: HackerShark <melsharkawi@mitre.org>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,7 +36,7 @@ function sidebarChildren(subDirPath)
 }
 
 module.exports = {
-    base: process.env.NODE_ENV === "production" ? "/inspec-developer/" : "/",
+    base: process.env.GITHUB_DEPLOY == true ? "/inspec-developer/" : "/",
     title: 'InSpec Profile Developers Course',
     description: "The MITRE InSpec Team's introduction to InSpec Profile Development",
 
